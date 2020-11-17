52f09bc5bdeb7719b3604d43455244e8f58f7206
SantiR38 <santirodriguez38@gmail.com>
2020-11-17 10:47:47 -0300

    ViewSet for Product model created.

--- clicoh/products/views.py
+++ clicoh/products/views.py
@@ -3,4 +3,18 @@
 # Django
 from django.shortcuts import render
 
+# Rest Framework
+from rest_framework import viewsets
 
+# Models
+from .models import Product
+
+# Serializers
+from .serializers import ProductSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """Product Model View Set."""
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
