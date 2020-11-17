2b4e85ffe876300731294f717847c67fd2da99f9
SantiR38 <santirodriguez38@gmail.com>
2020-11-17 00:16:56 -0300

    Models created and migrated.

--- clicoh/products/models.py
+++ clicoh/products/models.py
@@ -3,3 +3,9 @@
 # Django
 from django.db import models
 
+class Product(models.Model):
+    """Product model."""
+
+    id = models.CharField(max_length=20, primary_key=True)
+    name = models.CharField(max_length=25)
+    price = models.FloatField()
--- clicoh/sales/models.py
+++ clicoh/sales/models.py
@@ -3,3 +3,23 @@
 # Django
 from django.db import models
 
+
+class Order(models.Model):
+    """Order model.
+
+    Primary key will be created automatically and inevitably.
+    """
+
+    date_time = models.DateTimeField()
+
+
+class OrderDetail(models.Model):
+    """Order Detail model.
+
+    Primary key will be created automatically and inevitably.
+    """
+
+    order = models.ForeignKey('sales.Order', on_delete=models.CASCADE)
+    cuantity = models.IntegerField()
+    price = models.FloatField()
+    product = models.ForeignKey('products.Product', on_delete=models.CASCADE)
