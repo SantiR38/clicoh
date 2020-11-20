8b4abbcbee545a8aae1c462e0f50e6dbef0cfe39
SantiR38 <santirodriguez38@gmail.com>
2020-11-20 09:21:11 -0300

    Changed pagination.

--- config/settings/base.py
+++ config/settings/base.py
@@ -162,5 +162,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 2,
+    'PAGE_SIZE': 5,
 }
